86a623f3c1f8f452624ea588b7ee38d44dee43af
Martin <martinfrances107@hotmail.com>
2021-01-14 20:43:07 +0000

    Reestablished invert_test.rs inlight of Point to Coordinate conversion.

--- tests/invert_test.rs
+++ tests/invert_test.rs
@@ -1,43 +1,42 @@
-// #[cfg(not(tarpaulin_include))]
-// #[cfg(test)]
-// mod innvert_test {
-//     extern crate pretty_assertions;
+#[cfg(not(tarpaulin_include))]
+#[cfg(test)]
+mod innvert_test {
+    extern crate pretty_assertions;
 
-//     use delaunator::Point;
+    use geo::Coordinate;
+    use rust_d3_geo::projection::orthographic::OrthographicRaw;
+    use rust_d3_geo::projection::projection_equal::projection_equal;
+    use rust_d3_geo::projection::stereographic::StereographicRaw;
+    use rust_d3_geo::Transform;
 
-//     use rust_d3_geo::projection::orthographic::OrthographicRaw;
-//     use rust_d3_geo::projection::projection_equal::projection_equal;
-//     use rust_d3_geo::projection::stereographic::StereographicRaw;
-//     use rust_d3_geo::Transform;
+    #[test]
+    fn symetric_invert() {
+        let projectors = vec![
+            StereographicRaw::gen_projection_mutator(),
+            OrthographicRaw::gen_projection_mutator(),
+        ];
 
-//     #[test]
-//     fn symetric_invert() {
-//         let projectors = vec![
-//             StereographicRaw::gen_projection_mutator(),
-//             OrthographicRaw::gen_projection_mutator(),
-//         ];
-
-//         for pm in projectors {
-//             for p in vec![
-//                 Point {
-//                     x: 0.0f64,
-//                     y: 0.0f64,
-//                 },
-//                 Point {
-//                     x: 30.3f64,
-//                     y: 24.1f64,
-//                 },
-//                 Point {
-//                     x: -10f64,
-//                     y: 42f64,
-//                 },
-//                 Point {
-//                     x: -2.0f64,
-//                     y: -5.0f64,
-//                 },
-//             ] {
-//                 assert!(projection_equal(&pm, &p, &pm.transform(&p), None));
-//             }
-//         }
-//     }
-// }
+        for pm in projectors {
+            for p in vec![
+                Coordinate {
+                    x: 0.0f64,
+                    y: 0.0f64,
+                },
+                Coordinate {
+                    x: 30.3f64,
+                    y: 24.1f64,
+                },
+                Coordinate {
+                    x: -10f64,
+                    y: 42f64,
+                },
+                Coordinate {
+                    x: -2.0f64,
+                    y: -5.0f64,
+                },
+            ] {
+                assert!(projection_equal(&pm, &p, &pm.transform(&p), None));
+            }
+        }
+    }
+}
