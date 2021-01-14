ec841ec4f7f34a37c222f3c5ee00e43633a21868
Martin <martinfrances107@hotmail.com>
2021-01-14 21:05:50 +0000

    Reestablished stereographic_test after Point to Coordinate conversion.

--- tests/stereographic_test.rs
+++ tests/stereographic_test.rs
@@ -1,46 +1,46 @@
-// #[cfg(not(tarpaulin_include))]
-// #[cfg(test)]
-// mod stereographic_tests {
-//     use delaunator::Point;
-//     use rust_d3_geo::projection::projection::Projection;
-//     use rust_d3_geo::projection::projection_equal::projection_equal;
-//     use rust_d3_geo::projection::stereographic::StereographicRaw;
+#[cfg(not(tarpaulin_include))]
+#[cfg(test)]
+mod stereographic_tests {
+    use geo::Coordinate;
+    use rust_d3_geo::projection::projection::Projection;
+    use rust_d3_geo::projection::projection_equal::projection_equal;
+    use rust_d3_geo::projection::stereographic::StereographicRaw;
 
-//     #[test]
-//     fn test_stereographic() {
-//         let mut stereo = StereographicRaw::gen_projection_mutator();
-//         stereo.translate(Some(&Point { x: 0f64, y: 0f64 }));
-//         stereo.scale(Some(&1f64));
+    #[test]
+    fn test_stereographic() {
+        let mut stereo = StereographicRaw::gen_projection_mutator();
+        stereo.translate(Some(&Coordinate { x: 0f64, y: 0f64 }));
+        stereo.scale(Some(&1f64));
 
-//         assert!(projection_equal(
-//             &stereo,
-//             &Point { x: 0f64, y: 0f64 },
-//             &Point { x: 0f64, y: 0f64 },
-//             None
-//         ));
-//         assert!(projection_equal(
-//             &stereo,
-//             &Point { x: -90f64, y: 0f64 },
-//             &Point { x: -1f64, y: 0f64 },
-//             None
-//         ));
-//         assert!(projection_equal(
-//             &stereo,
-//             &Point { x: 90f64, y: 0f64 },
-//             &Point { x: 1f64, y: 0f64 },
-//             None
-//         ));
-//         assert!(projection_equal(
-//             &stereo,
-//             &Point { x: 0f64, y: -90f64 },
-//             &Point { x: 0f64, y: 1f64 },
-//             None
-//         ));
-//         assert!(projection_equal(
-//             &stereo,
-//             &Point { x: 0f64, y: 90f64 },
-//             &Point { x: 0f64, y: -1f64 },
-//             None
-//         ));
-//     }
-// }
+        assert!(projection_equal(
+            &stereo,
+            &Coordinate { x: 0f64, y: 0f64 },
+            &Coordinate { x: 0f64, y: 0f64 },
+            None
+        ));
+        assert!(projection_equal(
+            &stereo,
+            &Coordinate { x: -90f64, y: 0f64 },
+            &Coordinate { x: -1f64, y: 0f64 },
+            None
+        ));
+        assert!(projection_equal(
+            &stereo,
+            &Coordinate { x: 90f64, y: 0f64 },
+            &Coordinate { x: 1f64, y: 0f64 },
+            None
+        ));
+        assert!(projection_equal(
+            &stereo,
+            &Coordinate { x: 0f64, y: -90f64 },
+            &Coordinate { x: 0f64, y: 1f64 },
+            None
+        ));
+        assert!(projection_equal(
+            &stereo,
+            &Coordinate { x: 0f64, y: 90f64 },
+            &Coordinate { x: 0f64, y: -1f64 },
+            None
+        ));
+    }
+}
