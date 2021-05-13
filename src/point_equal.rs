ff752a53462063d5cbf3a15f98fd5024a49bf4c9
Martin <martinfrances107@hotmail.com>
2021-05-13 06:32:18 +0100

    BugFix point_equals.rs

--- src/point_equal.rs
+++ src/point_equal.rs
@@ -1,10 +1,9 @@
 use geo::{CoordFloat, Coordinate};
 
-// use crate::math::EPSILON;
-
 /// An aspect of the javascrtipt
 /// point_equal may be fed 3 floats but only checks values 0 and 1. (so not all) !!!
 #[inline]
 pub fn point_equal<T: CoordFloat>(a: Coordinate<T>, b: Coordinate<T>) -> bool {
-    (a.x - b.x.abs() < T::epsilon()) && ((a.y - b.y).abs() < T::epsilon())
+    let epsilon: T = T::from(1e-6).unwrap();
+    ((a.x - b.x).abs() < epsilon) && ((a.y - b.y).abs() < epsilon)
 }
