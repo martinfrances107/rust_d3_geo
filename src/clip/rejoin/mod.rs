b6e2b75de345f9e38d42f8422b7affa8d76136bb
Martin <martinfrances107@hotmail.com>
2021-05-15 14:02:22 +0100

    Simplification: rejoin() within the nested loops, regarding the variable current -- Option was removed from definition of current it was  Option<Rc<RefMut<Intersection<T>>> -- After calls to link() by design all source that set current come from a valid fully formed Rc<RefMut<Intersection<T>>>.

--- src/clip/rejoin/mod.rs
+++ src/clip/rejoin/mod.rs
@@ -10,7 +10,6 @@ use geo::CoordFloat;
 use geo::Coordinate;
 use num_traits::AsPrimitive;
 use num_traits::FloatConst;
-use num_traits::Zero;
 
 use crate::point_equal::point_equal;
 use crate::stream::Stream;
@@ -32,8 +31,8 @@ pub fn rejoin<T>(
 ) where
     T: AddAssign + AsPrimitive<T> + CoordFloat + Default + Display + FloatConst,
 {
+    println!("rejoin - startInside {:?}", start_inside);
     println!("clip rejoin: segments: {:#?}", segments);
-    // panic!("just entered rejoin");
     let mut start_inside = start_inside;
     let mut subject = Vec::<Rc<RefCell<Intersection<T>>>>::new();
     let mut clip = Vec::<Rc<RefCell<Intersection<T>>>>::new();
@@ -59,7 +58,6 @@ pub fn rejoin<T>(
                 return;
             }
             // handle degenerate cases by moving the point
-            // p1[0] += 2F * f64::EPSILON;
             p1.p.x += T::from(2.0 * 1e-6).unwrap();
         }
 
@@ -71,16 +69,13 @@ pub fn rejoin<T>(
         )));
         subject.push(x1.clone());
 
-        let other1 = Rc::new(RefCell::new(Intersection::new(
+        (*x1).borrow_mut().o = Some(Rc::new(RefCell::new(Intersection::new(
             p0,
             None,
             Some(x1.clone()),
             false,
-        )));
-        (*x1).borrow_mut().o = Some(other1.clone());
-
-        x1.borrow_mut().o = Some(other1.clone());
-        clip.push(other1);
+        ))));
+        clip.push((*x1).borrow().o.clone().unwrap());
 
         let x2 = Rc::new(RefCell::new(Intersection::new(
             p1,
@@ -89,30 +84,34 @@ pub fn rejoin<T>(
             false,
         )));
         subject.push(x2.clone());
-        let other2 = Rc::new(RefCell::new(Intersection::new(
+        (*x2).borrow_mut().o = Some(Rc::new(RefCell::new(Intersection::new(
             p1,
             None,
             Some(x1.clone()),
             true,
-        )));
-        (*x2).borrow_mut().o = Some(other2.clone());
-
-        clip.push(other2);
+        ))));
+        clip.push((*x2).borrow().o.clone().unwrap());
     }
 
     if subject.is_empty() {
         return;
     }
 
+    println!("clip before sort {:#?}", clip);
     clip.sort_by(ClipRaw::compare_intersection);
+    println!("clip after sort {:#?}", clip);
 
     link(&mut subject);
     link(&mut clip);
 
     for i in 0..clip.len() {
+        println!("start inside loop {:?} {:?}", i, start_inside);
         start_inside = !start_inside;
         (*clip[i]).borrow_mut().e = start_inside;
     }
+
+    // println!("edge on clip");
+    // println!("{:#?}", clip);
 
     let start = &subject[0];
     // let points: Vec<LineElem<T>>;
@@ -120,46 +119,52 @@ pub fn rejoin<T>(
 
     loop {
         // Find first unvisited intersection.
-        let mut current: Option<Rc<RefCell<Intersection<T>>>> = Some(start.clone());
+        let mut current: Rc<RefCell<Intersection<T>>> = start.clone();
         let mut is_subject = true;
 
-        while current.clone().unwrap().borrow().v {
-            current = match current.clone() {
-                Some(c) => c.borrow().n.clone(),
-                None => None,
-            };
-
-            match current.clone() {
-                Some(c) => {
-                    // todo!("must implement compare.");
-                    // panic!("about to compare. c {:?} start {:?}", c, start);
-                    if *c.borrow() == *start.borrow() {
-                        return;
-                    }
-                }
-                None => {
-                    panic!("compare with none");
-                }
+        let mut loop_count = 0;
+        println!("---------------------------------------------------");
+        println!("{:?}", current);
+        // panic!("here");
+        while current.borrow().v {
+            println!("enter first unvisited loop");
+            current = current.clone().borrow().n.clone().unwrap();
+            if *current.borrow() == *start.borrow() {
+                return;
             }
+
+            if loop_count > 10 {
+                panic!("loop count exceeded.");
+            }
+            loop_count = loop_count + 1;
+            println!("updated current {:?}", current);
         }
-        let mut points = (current.clone().unwrap()).borrow().z.clone();
+
+        let mut points = current.borrow().z.clone();
 
         stream.line_start();
+        let mut mid_loop_count = 0;
         loop {
-            match &mut (current.clone().unwrap()).borrow_mut().o {
-                Some(o) => {
-                    (*o).borrow_mut().v = true;
-                }
-                None => {
-                    panic!("Cannot reach into blank and set true");
-                }
+            if mid_loop_count > 5 {
+                panic!("mid loop count exceeded");
             }
-            (current.clone().unwrap()).borrow_mut().v = true;
+            mid_loop_count = mid_loop_count + 1;
+            println!("enter mid loop");
 
-            if (current.clone().unwrap()).borrow().e {
+            current.borrow().o.clone().unwrap().borrow_mut().v = true;
+            current.borrow_mut().v = true;
+
+            println!("marking current as visited other ");
+            println!("{:#?}", current.borrow().o.clone().unwrap().borrow());
+            println!("marking current as visited");
+            println!("{:#?}", current.borrow().clone());
+            println!("about to current.e ");
+            if current.borrow().e {
+                println!("subject first is_subject");
                 if is_subject {
                     match points {
                         Some(points) => {
+                            println!("about to 4");
                             for i in 0..points.len() {
                                 point = points[i];
                                 stream.point(&point.p, point.m);
@@ -169,26 +174,19 @@ pub fn rejoin<T>(
                     }
                 } else {
                     raw.interpolate(
-                        Some((current.clone().unwrap()).borrow().x.p),
-                        Some(
-                            (current.clone().unwrap())
-                                .borrow()
-                                .n
-                                .as_ref()
-                                .unwrap()
-                                .borrow()
-                                .x
-                                .p,
-                        ),
+                        Some((current.clone()).borrow().x.p),
+                        Some((current.clone()).borrow().n.as_ref().unwrap().borrow().x.p),
                         T::one(),
                         stream,
                     );
                 }
-                let next = (*current.unwrap()).borrow().n.clone();
-                current = next;
+                println!("about to assign current from n");
+                current = current.clone().borrow().n.clone().unwrap();
             } else {
+                panic!("bad path ");
+                println!("subject -- else");
                 if is_subject {
-                    points = (*(*current.clone().unwrap()).borrow().p.as_ref().unwrap())
+                    points = (*(*current.clone()).borrow().p.as_ref().unwrap())
                         .borrow()
                         .z
                         .clone();
@@ -198,9 +196,9 @@ pub fn rejoin<T>(
                     }
                 } else {
                     raw.interpolate(
-                        Some((*current.clone().unwrap()).borrow().x.p),
+                        Some((*current.clone()).borrow().x.p),
                         Some(
-                            ((current.clone().unwrap()).borrow().p.as_ref().unwrap())
+                            ((current.clone()).borrow().p.as_ref().unwrap())
                                 .borrow()
                                 .x
                                 .p,
@@ -209,43 +207,24 @@ pub fn rejoin<T>(
                         stream,
                     );
                 }
+                println!("assigning current from p");
+                current = current.clone().borrow().p.clone().unwrap();
             }
 
-            current = Some(
-                current
-                    .clone()
-                    .unwrap()
-                    .borrow()
-                    .p
-                    .as_ref()
-                    .unwrap()
-                    .clone(),
-            );
-            current = match &current.clone().unwrap().borrow().p {
-                Some(c) => Some(c.clone()),
-                None => None,
-            };
+            // println!("about to set current(o) {:#?}", current);
+            println!("set current o");
 
-            points = match current.clone() {
-                Some(c) => c.borrow().z.clone(),
-                None => None,
-            };
+            current = current.clone().borrow().o.clone().unwrap();
+            points = current.clone().borrow().z.clone();
+
             is_subject = !is_subject;
 
-            // if !(*current.clone().unwrap()).borrow().v {
-            //     break;
-            // }
-            match current {
-                Some(ref c) => {
-                    if !(*c).borrow().v {
-                        break;
-                    }
-                }
-                None => {
-                    break;
-                }
+            if current.borrow().v == true {
+                break;
             }
         }
+
+        println!("exit loops rejoin about to line_end");
         stream.line_end();
     }
 }
