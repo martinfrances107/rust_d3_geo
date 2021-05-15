61cf04909e700ab956d73122bf98f2a0dc00a099
Martin <martinfrances107@hotmail.com>
2021-05-15 13:54:20 +0100

    BugFix: link() now correctly form a circular link.

--- src/clip/rejoin/link.rs
+++ src/clip/rejoin/link.rs
@@ -6,6 +6,10 @@ use num_traits::FloatConst;
 
 use super::intersection::Intersection;
 
+/// Given a vec of Intersetions :-
+///
+/// Set the next and pervious entries to be the elements above and
+/// below. Connect the ends to form a circular loop.
 pub(super) fn link<T>(array: &mut Vec<Rc<RefCell<Intersection<T>>>>)
 where
     T: CoordFloat + FloatConst,
@@ -20,9 +24,10 @@ where
     for i in 1..n {
         b = array[i].clone();
         (*a).borrow_mut().n = Some(b.clone());
-        (*b).borrow_mut().p = Some(a);
-        a = b.clone();
+        (*b).borrow_mut().p = Some(a.clone());
+        a = b;
     }
     b = array[0].clone();
-    (*a).borrow_mut().n = Some(b);
+    (*a).borrow_mut().n = Some(b.clone());
+    (*b).borrow_mut().p = Some(a);
 }
