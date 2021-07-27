3a796edcb4462b6d8d5bef1466f1f81de31cf7b0
Martin <martinfrances107@hotmail.com>
2021-07-27 14:20:29 +0100

    Removed clip_macro/target from the repo.

--- clip_macro/target/debug/deps/clip_macro-d8e95b697e5a3107.d
+++
@@ -1,5 +0,0 @@
-/home/martin/build/rust_d3_geo/clip_macro/target/debug/deps/clip_macro-d8e95b697e5a3107: src/lib.rs
-
-/home/martin/build/rust_d3_geo/clip_macro/target/debug/deps/clip_macro-d8e95b697e5a3107.d: src/lib.rs
-
-src/lib.rs:
--- clip_macro/target/release/libclip_macro.d
+++
@@ -1,1 +0,0 @@
-/home/martin/build/rust_d3_geo/clip_macro/target/release/libclip_macro.so: /home/martin/build/rust_d3_geo/clip_macro/src/lib.rs
