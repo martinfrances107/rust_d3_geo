25dc62920a3a2e212746e812fcc912898c52701d
Martin <martinfrances107@hotmail.com>
2021-08-23 21:45:22 +0100

    Minor: Simplified circle/line.rs

--- src/clip/circle/line.rs
+++ src/clip/circle/line.rs
@@ -163,12 +163,13 @@ where
         };
         println!("clip:circle point entry v, c {:?} {:?}", v, c);
         println!("clip:circle point self.point0 {:?}", self.raw.point0);
+        let mut s = self.sink.borrow_mut();
         if self.raw.point0.is_none() {
             self.raw.v00 = v;
             self.raw.v0 = v;
             println!("setting v ");
             if v {
-                self.sink.borrow_mut().line_start();
+                s.line_start();
             }
         }
         println!("before intersect check {:?} {:?}", v, self.raw.v0);
@@ -206,7 +207,7 @@ where
             if v {
                 println!("outside going in");
                 // outside going in
-                self.sink.borrow_mut().line_start();
+                s.line_start();
                 point2 = match intersect(
                     &point1.unwrap(),
                     &self.raw.point0.unwrap(),
@@ -222,7 +223,7 @@ where
                         todo!("must cover this case.");
                     }
                 };
-                self.sink.borrow_mut().point(&point2.unwrap().p, None)
+                s.point(&point2.unwrap().p, None)
             } else {
                 // inside going out
                 println!("inside going out");
@@ -242,8 +243,8 @@ where
                     }
                 };
                 // panic!("about to insert 2 ");
-                self.sink.borrow_mut().point(&point2.unwrap().p, Some(2));
-                self.sink.borrow_mut().line_end();
+                s.point(&point2.unwrap().p, Some(2));
+                s.line_end();
             }
             self.raw.point0 = point2;
         } else if self.raw.not_hemisphere && self.raw.point0.is_some() && self.raw.small_radius ^ v
@@ -272,16 +273,16 @@ where
                         self.raw.clean = CleanEnum::IntersectionsOrEmpty;
                         if self.raw.small_radius {
                             println!("small radius");
-                            self.sink.borrow_mut().line_start();
-                            self.sink.borrow_mut().point(&t[0], None);
-                            self.sink.borrow_mut().point(&t[1], None);
-                            self.sink.borrow_mut().line_end();
+                            s.line_start();
+                            s.point(&t[0], None);
+                            s.point(&t[1], None);
+                            s.line_end();
                         } else {
                             println!("not a small radius");
-                            self.sink.borrow_mut().point(&t[1], None);
-                            self.sink.borrow_mut().line_end();
-                            self.sink.borrow_mut().line_start();
-                            self.sink.borrow_mut().point(&t[0], Some(3_u8));
+                            s.point(&t[1], None);
+                            s.line_end();
+                            s.line_start();
+                            s.point(&t[0], Some(3_u8));
                         }
                     }
                 }
@@ -292,7 +293,7 @@ where
             || !point_equal(self.raw.point0.unwrap().p, point1.unwrap().p))
         {
             println!("point equal pass");
-            self.sink.borrow_mut().point(&point1.unwrap().p, None);
+            s.point(&point1.unwrap().p, None);
         }
         self.raw.point0 = point1;
         self.raw.v0 = v;
