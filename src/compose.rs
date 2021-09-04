d59fd81efd5adbfd17f480a426194be9b5237f60
Martin <martinfrances107@hotmail.com>
2021-09-04 19:25:32 +0100

    Removed StreamNode for Composer it is not used.

--- src/compose.rs
+++ src/compose.rs
@@ -1,10 +1,9 @@
 use geo::{CoordFloat, Coordinate};
 use num_traits::FloatConst;
 
-use crate::projection::stream_node::StreamNode;
-use crate::stream::Stream;
 use crate::Transform;
 
+/// Combines transform together.
 #[derive(Clone, Copy, Debug)]
 pub struct Compose<T, TA, TB>
 where
@@ -45,45 +44,5 @@ where
     fn invert(&self, coordinate: &Coordinate<T>) -> Coordinate<T> {
         let temp = self.b.invert(coordinate);
         self.a.invert(&temp)
-    }
-}
-
-impl<SINK, T, TA, TB> Stream for StreamNode<Compose<T, TA, TB>, SINK, T>
-where
-    SINK: Stream<T = T>,
-    TA: Transform<T = T>,
-    TB: Transform<T = T>,
-    T: CoordFloat + FloatConst,
-{
-    type T = T;
-
-    #[inline]
-    fn point(&mut self, p: &Coordinate<T>, m: Option<u8>) {
-        self.sink.borrow_mut().point(&self.raw.transform(p), m);
-    }
-
-    #[inline]
-    fn sphere(&mut self) {
-        self.sink.borrow_mut().sphere();
-    }
-
-    #[inline]
-    fn line_start(&mut self) {
-        self.sink.borrow_mut().line_start();
-    }
-
-    #[inline]
-    fn line_end(&mut self) {
-        self.sink.borrow_mut().line_end();
-    }
-
-    #[inline]
-    fn polygon_start(&mut self) {
-        self.sink.borrow_mut().polygon_start()
-    }
-
-    #[inline]
-    fn polygon_end(&mut self) {
-        self.sink.borrow_mut().polygon_end();
     }
 }
