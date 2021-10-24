70e5dc80ab6292afa67069e6aef1d9f682c95716
Martin <martinfrances107@hotmail.com>
2021-10-24 11:07:57 +0100

    Added performance note. I think in a critial path we could remove a copy.

--- src/stream/polygon.rs
+++ src/stream/polygon.rs
@@ -9,12 +9,14 @@ impl<T> Streamable for Polygon<T>
 where
     T: CoordFloat,
 {
-    // type T=T;
-    // type SD = Self;
     type T = T;
+
     fn to_stream<SD: Stream<T = T>>(&self, stream: &mut SD) {
         stream.polygon_start();
 
+        // Performance: There is a conversion here and a copy which I think is slow.
+        // If I can pass in a Iterators, get the length without copy then
+        // a frequently called copy would be avoided.
         let e_points: Vec<Coordinate<T>> = self.exterior().coords_iter().collect();
         stream_line(&e_points, stream, 1);
 
