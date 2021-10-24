a4befad8a71dfb96ad55703ea7a3be3501a40ce1
Martin <martinfrances107@hotmail.com>
2021-10-24 13:46:56 +0100

    Added note saying stream_line() might need a refactor using generics to take both a Coordinaate and a LineElem.

--- src/stream/stream_line.rs
+++ src/stream/stream_line.rs
@@ -2,6 +2,8 @@ use geo::{CoordFloat, Coordinate};
 
 use super::Stream;
 
+/// TODO Generics - Need to come back and refactor to take LineElem<T>
+/// or Coordinates. As the JS allow for.
 pub fn stream_line<T, S>(coordinates: &[Coordinate<T>], stream: &mut S, closed: usize)
 where
     S: Stream<T = T>,
