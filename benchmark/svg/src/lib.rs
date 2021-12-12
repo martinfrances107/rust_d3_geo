e5c4c648e877b57623d301498e55f257a60a9552
Martin <martinfrances107@hotmail.com>
2021-12-12 21:58:44 +0000

    Now pulling svg.width and height. Plus clippy changes.

--- benchmark/svg/src/lib.rs
+++ benchmark/svg/src/lib.rs
@@ -14,7 +14,6 @@ extern crate rand;
 extern crate rust_topojson_client;
 extern crate topojson;
 extern crate web_sys;
-use std::rc::Rc;
 
 use geo::Coordinate;
 use rust_topojson_client::feature::Builder as FeatureBuilder;
@@ -26,11 +25,8 @@ use web_sys::Document;
 use web_sys::SvgsvgElement;
 use web_sys::*;
 
-use rust_d3_geo::clip::circle::line::Line;
-use rust_d3_geo::clip::circle::pv::PV;
 use rust_d3_geo::data_object::DataObject;
 use rust_d3_geo::path::builder::Builder as PathBuilder;
-use rust_d3_geo::path::context::Context;
 use rust_d3_geo::path::context_stream::ContextStream;
 use rust_d3_geo::path::ResultEnum;
 use rust_d3_geo::projection::orthographic::Orthographic;
@@ -44,46 +40,15 @@ use topojson::Topology;
 
 mod dom_macros;
 
-#[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-
-    // The `console.log` is quite polymorphic, so we can bind it with multiple
-    // signatures. Note that we need to use `js_name` to ensure we always call
-    // `log` in JS.
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_u32(a: u32);
-
-    // Multiple arguments too!
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_many(a: &str, b: &str);
-
-    fn alert(s: &str);
-}
-
-// Next let's define a macro that's like `println!`, only it works for
-// `console.log`. Note that `println!` doesn't actually work on the wasm target
-// because the standard library currently just eats all output. To get
-// `println!`-like behavior in your app you'll likely want a macro like this.
-
-macro_rules! console_log {
-    // Note that this is using the `log` function imported above during
-    // `bare_bones`
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
-
 fn get_document() -> Result<Document, JsValue> {
     let window = web_sys::window().unwrap();
     Ok(window.document().unwrap())
 }
 
-/// Entry point
+/// Entry point.
 #[wasm_bindgen(start)]
 pub async fn start() -> Result<(), JsValue> {
-    console_log!("run() - wasm entry point");
+    log_1(&JsValue::from("run() - wasm entry point"));
     let document = get_document()?;
 
     let mut opts = RequestInit::new();
@@ -91,7 +56,7 @@ pub async fn start() -> Result<(), JsValue> {
     opts.mode(RequestMode::Cors);
 
     // let request = Request::new_with_str_and_init(&"/world-atlas/world/50m.json", &opts)?;
-    let request = Request::new_with_str_and_init(&"/world-atlas/africa.json", &opts)?;
+    let request = Request::new_with_str_and_init("/world-atlas/africa.json", &opts)?;
 
     request.headers().set("Accept", "application/json")?;
 
@@ -113,37 +78,16 @@ pub async fn start() -> Result<(), JsValue> {
         .unwrap()
         .dyn_into::<web_sys::SvgsvgElement>()?;
 
-    // let width: f64 = svg.width().into();
-    // let height: f64 = svg.height().into();
-    let width = 800_f64;
-    let height = 600_f64;
+    let width = svg.width().base_val().value()? as f64;
+    let height = svg.height().base_val().value()? as f64;
 
     // let land = FeatureBuilder::<f64>::generate_from_name(&topology, &"land")
-    let land = FeatureBuilder::<f64>::generate_from_name(&topology, &"countries")
+    let land = FeatureBuilder::<f64>::generate_from_name(&topology, "countries")
         .expect("Did not extract geometry");
 
     let object = DataObject::Geometry(land);
-    // match FeatureBuilder::<f64>::generate_from_name(&topology, &"land") {
-    //     Some(Geometry::GeometryCollection(GeometryCollection(v_geometry))) => {
-    //         assert_eq!(v_geometry.len(), 1);
-    //         match &v_geometry[0] {
-    //             Geometry::MultiPolygon(mp) => {
-    //                 assert_eq!(mp.0.len(), 1428_usize);
-    //                 let v_polygon = mp.0;
-    //             }
-    //             _ => {
-    //                 assert!(false, "Failed to decode Multipoloygon")
-    //             }
-    //         }
-    //     }
-    //     _ => {
-    //         assert!(false, "failed to extract a vector of geometries");
-    //     }
-    // };
-
     let ortho_builder = Orthographic::<ContextStream<f64>, f64>::builder();
 
-    // ortho_builder.scale();
     let ortho = ortho_builder
         .scale(width as f64 / 1.3_f64 / std::f64::consts::PI)
         .translate(&Coordinate {
@@ -155,22 +99,12 @@ pub async fn start() -> Result<(), JsValue> {
 
     let builder = PathBuilder::context_pathstring();
 
-    // let result = builder.build(projection).object(&object);
-    let path_d = match builder.build(ortho).object(&object) {
-        Some(r) => match r {
-            ResultEnum::String(s) => s,
-            _ => String::from(""),
-        },
-        None => String::from(""),
-    };
-
-    log_1(&JsValue::from(path_d.clone()));
-
-    let e: Element;
-    if let Ok(path) = document.create_element_ns(Some("http://www.w3.org/2000/svg"), &"path") {
-        path.set_attribute_ns(None, "d", &path_d);
-        // path.d(path_d);
-        svg.append_child(&path);
+    if let Some(ResultEnum::String(path_d)) = builder.build(ortho).object(&object) {
+        log_1(&JsValue::from(path_d.clone()));
+        if let Ok(path) = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "path") {
+            path.set_attribute_ns(None, "d", &path_d)?;
+            svg.append_child(&path)?;
+        }
     }
 
     Ok(())
