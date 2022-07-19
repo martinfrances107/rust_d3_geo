6a330a47caaf9d6e9165c1f974f3b4357f344c71
Martin <martinfrances107@hotmail.com>
2022-07-19 17:34:16 +0100

    Minor: removed clone over-constraints from build.rs

--- lib/src/projection/builder/build.rs
+++ lib/src/projection/builder/build.rs
@@ -7,42 +7,40 @@ use crate::projection::Projector;
 use crate::stream::Unconnected;
 
 impl<DRAIN, I, LC, LB, LU, PCNU, PR, PV, RC, RU, T> Build
-	for Builder<DRAIN, I, LB, LC, LU, PCNU, PR, PV, RC, RU, T>
+    for Builder<DRAIN, I, LB, LC, LU, PCNU, PR, PV, RC, RU, T>
 where
-	DRAIN: Clone,
-	I: Clone,
-	LB: Clone,
-	LC: Clone,
-	LU: Clone,
-	PCNU: Clone,
-	PV: Clone,
-	PR: Clone,
-	RC: Clone,
-	RU: Clone,
-	T: CoordFloat,
+    I: Clone,
+    LC: Clone,
+    LU: Clone,
+    PCNU: Clone,
+    PV: Clone,
+    PR: Clone,
+    RC: Clone,
+    RU: Clone,
+    T: CoordFloat,
 {
-	type Drain = DRAIN;
-	type I = I;
-	type LB = LB;
-	type LC = LC;
-	type LU = LU;
-	type PCNU = PCNU;
-	type PR = PR;
-	type PV = PV;
-	type RC = RC;
-	type RU = RU;
-	type T = T;
-	/// Using the currently programmed state output a new projection.
-	#[inline]
-	fn build(&self) -> Projector<DRAIN, I, LB, LC, LU, PCNU, PR, PV, RC, RU, T> {
-		Projector {
-			cache: None,
-			postclip: self.postclip.clone(),
-			clip: self.clip.clone(),
-			resample: self.resample.clone(),
-			rotator: self.rotator.clone(),
-			project_rotate_transform: self.project_rotate_transform.clone(),
-			transform_radians: StreamTransformRadians(Unconnected),
-		}
-	}
+    type Drain = DRAIN;
+    type I = I;
+    type LB = LB;
+    type LC = LC;
+    type LU = LU;
+    type PCNU = PCNU;
+    type PR = PR;
+    type PV = PV;
+    type RC = RC;
+    type RU = RU;
+    type T = T;
+    /// Using the currently programmed state output a new projection.
+    #[inline]
+    fn build(&self) -> Projector<DRAIN, I, LB, LC, LU, PCNU, PR, PV, RC, RU, T> {
+        Projector {
+            cache: None,
+            postclip: self.postclip.clone(),
+            clip: self.clip.clone(),
+            resample: self.resample.clone(),
+            rotator: self.rotator.clone(),
+            project_rotate_transform: self.project_rotate_transform.clone(),
+            transform_radians: StreamTransformRadians(Unconnected),
+        }
+    }
 }
