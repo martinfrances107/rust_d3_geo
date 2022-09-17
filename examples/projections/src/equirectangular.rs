e10d04ab86a28b2a15432017b36ed4a595aa284b
Martin <martinfrances107@hotmail.com>
2022-09-17 08:43:31 +0100

    Minor: ran cargo fmt on examples.

--- examples/projections/src/equirectangular.rs
+++ examples/projections/src/equirectangular.rs
@@ -18,48 +18,48 @@ use rust_d3_geo::projection::TranslateSet;
 use crate::document;
 
 pub async fn draw_equirectangular(land: &Geometry<f64>) -> Result<(), JsValue> {
-	let document = document()?;
-	// Grab canvas.
-	let canvas = document
-		.get_element_by_id("equirectangular-rust")
-		.unwrap()
-		.dyn_into::<web_sys::HtmlCanvasElement>()?;
+    let document = document()?;
+    // Grab canvas.
+    let canvas = document
+        .get_element_by_id("equirectangular-rust")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
 
-	let context_raw = canvas
-		.get_context("2d")?
-		.unwrap()
-		.dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+    let context_raw = canvas
+        .get_context("2d")?
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
 
-	let context = context_raw.clone();
+    let context = context_raw.clone();
 
-	let width: f64 = canvas.width().into();
-	let height: f64 = canvas.height().into();
+    let width: f64 = canvas.width().into();
+    let height: f64 = canvas.height().into();
 
-	let context = Context::new(context);
-	let pb = PathBuilder::new(context);
+    let context = Context::new(context);
+    let pb = PathBuilder::new(context);
 
-	let equirectangular = Equirectangular::builder()
-		.scale_set(width as f64 / 1.5_f64 / std::f64::consts::PI)
-		.rotate_set(&[0_f64, 0_f64, 0_f64])
-		.center_set(&Coordinate { x: 0_f64, y: 0_f64 })
-		.translate_set(&Coordinate {
-			x: width / 2_f64,
-			y: height / 2_f64,
-		})
-		.build();
+    let equirectangular = Equirectangular::builder()
+        .scale_set(width as f64 / 1.5_f64 / std::f64::consts::PI)
+        .rotate_set(&[0_f64, 0_f64, 0_f64])
+        .center_set(&Coordinate { x: 0_f64, y: 0_f64 })
+        .translate_set(&Coordinate {
+            x: width / 2_f64,
+            y: height / 2_f64,
+        })
+        .build();
 
-	let mut path = pb.build(equirectangular);
-	context_raw.set_stroke_style(&"#69b3a2".into());
-	path.object(land);
-	context_raw.stroke();
+    let mut path = pb.build(equirectangular);
+    context_raw.set_stroke_style(&"#69b3a2".into());
+    path.object(land);
+    context_raw.stroke();
 
-	let lines = generate_graticule().lines();
-	let mls = Geometry::MultiLineString(MultiLineString(lines.collect()));
-	context_raw.begin_path();
-	context_raw.set_fill_style(&"#999".into());
-	context_raw.set_stroke_style(&"#69b3a2".into());
-	path.object(&mls);
-	context_raw.stroke();
+    let lines = generate_graticule().lines();
+    let mls = Geometry::MultiLineString(MultiLineString(lines.collect()));
+    context_raw.begin_path();
+    context_raw.set_fill_style(&"#999".into());
+    context_raw.set_stroke_style(&"#69b3a2".into());
+    path.object(&mls);
+    context_raw.stroke();
 
-	Ok(())
+    Ok(())
 }
