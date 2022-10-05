0fa5b32e6c4b9aee7fb40a395fc34108df0ca31c
Martin <martinfrances107@hotmail.com>
2022-10-05 16:53:54 +0100

    Minor: Clippy now using if let Some(clip) = clip to side step a .unwrap().

--- lib/src/projection/fit_reclip.rs
+++ lib/src/projection/fit_reclip.rs
@@ -65,7 +65,7 @@ pub(super) fn fit_reclip<B, CLIPC, CLIPU, PR, RC, RU, T>(
             x: T::zero(),
             y: T::zero(),
         });
-    if clip.is_some() {
+    if let Some(clip) = clip {
         let mut b = bprime.clip_extent_clear();
         let mut projector = b.build();
         let bounds_stream = Bounds::default();
@@ -73,7 +73,7 @@ pub(super) fn fit_reclip<B, CLIPC, CLIPU, PR, RC, RU, T>(
         object.to_stream(&mut stream_in);
         let bounds = stream_in.endpoint().result();
         fit_bounds(bounds, &mut b);
-        b.clip_extent_adjust(&clip.unwrap());
+        b.clip_extent_adjust(&clip);
     } else {
         let b = bprime;
         let mut projector = b.build();
