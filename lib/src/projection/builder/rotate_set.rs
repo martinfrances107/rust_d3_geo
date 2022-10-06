6fb2a644660d7184d9373aa03309887207fd2db0
Martin <martinfrances107@hotmail.com>
2022-10-06 22:34:03 +0100

    Rotate set was missing two variations.

--- lib/src/projection/builder/rotate_set.rs
+++ lib/src/projection/builder/rotate_set.rs
@@ -3,12 +3,17 @@ use num_traits::FloatConst;
 
 use crate::projection::builder::ResampleNoPCNU;
 use crate::projection::builder::ResamplePCNU;
+use crate::projection::RecenterNoResampling;
 use crate::projection::RecenterWithResampling;
 use crate::projection::RotateSet;
 use crate::Transform;
 
 use super::template::NoPCNU;
 use super::template::ResampleNoPCNC;
+use super::template::ResampleNoneNoPCNC;
+use super::template::ResampleNoneNoPCNU;
+use super::template::ResampleNonePCNC;
+use super::template::ResampleNonePCNU;
 use super::template::ResamplePCNC;
 use super::template::PCNU;
 use super::Builder;
@@ -72,5 +77,67 @@ where
         self.delta_phi = (delta_phi % f360).to_radians();
         self.delta_gamma = (delta_gamma % f360).to_radians();
         self.recenter_with_resampling()
+    }
+}
+
+impl<CLIPC, CLIPU, DRAIN, PR, T> RotateSet
+    for Builder<
+        CLIPC,
+        CLIPU,
+        DRAIN,
+        NoPCNU<DRAIN>,
+        PR,
+        ResampleNoneNoPCNC<DRAIN, PR, T>,
+        ResampleNoneNoPCNU<DRAIN, PR, T>,
+        T,
+    >
+where
+    CLIPC: Clone,
+    CLIPU: Clone,
+    DRAIN: Clone,
+    PR: Clone + Transform<T = T>,
+    T: CoordFloat + FloatConst,
+{
+    type T = T;
+
+    /// Sets the rotation angles as measured in degrees.
+    fn rotate_set(&mut self, angles: &[T; 3]) -> &mut Self {
+        let [delta_lambda, delta_phi, delta_gamma] = *angles;
+        let f360 = T::from(360_f64).unwrap();
+        self.delta_lambda = (delta_lambda % f360).to_radians();
+        self.delta_phi = (delta_phi % f360).to_radians();
+        self.delta_gamma = (delta_gamma % f360).to_radians();
+        self.recenter_no_resampling()
+    }
+}
+
+impl<CLIPC, CLIPU, DRAIN, PR, T> RotateSet
+    for Builder<
+        CLIPC,
+        CLIPU,
+        DRAIN,
+        PCNU<DRAIN, T>,
+        PR,
+        ResampleNonePCNC<DRAIN, PR, T>,
+        ResampleNonePCNU<DRAIN, PR, T>,
+        T,
+    >
+where
+    CLIPC: Clone,
+    CLIPU: Clone,
+    DRAIN: Clone,
+    PR: Clone + Transform<T = T>,
+    T: CoordFloat + FloatConst,
+{
+    type T = T;
+
+    /// Sets the rotation angles as measured in degrees.
+    fn rotate_set(&mut self, angles: &[T; 3]) -> &mut Self {
+        let [delta_lambda, delta_phi, delta_gamma] = *angles;
+        let f360 = T::from(360_f64).unwrap();
+        self.delta_lambda = (delta_lambda % f360).to_radians();
+        self.delta_phi = (delta_phi % f360).to_radians();
+        self.delta_gamma = (delta_gamma % f360).to_radians();
+        self.recenter_no_resampling()
     }
 }
