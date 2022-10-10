9fb4b7ae636ce4c7932f5ee9ddead53d1509d7f7
Martin <martinfrances107@hotmail.com>
2022-10-10 20:30:18 +0100

    precision_get was too permissive. API state design should prevent calling .precision() on a builder that involves ResampleNone.

--- lib/src/projection/builder_mercator/precision_get.rs
+++ lib/src/projection/builder_mercator/precision_get.rs
@@ -1,15 +1,60 @@
+use approx::AbsDiffEq;
 use geo::CoordFloat;
+use num_traits::FloatConst;
 
 use crate::projection::PrecisionGet;
 
+use crate::clip::antimeridian::ClipAntimeridianC;
+use crate::clip::antimeridian::ClipAntimeridianU;
+use crate::clip::circle::ClipCircleC;
+use crate::clip::circle::ClipCircleU;
+use crate::projection::resampler::resample::Connected as ConnectedResample;
+use crate::projection::resampler::resample::Resample;
+use crate::stream::Unconnected;
+use crate::Transform;
+
 use super::Builder;
 
-impl<CLIPC, CLIPU, DRAIN, PCNU, PR, RC, RU, T> PrecisionGet
-    for Builder<CLIPC, CLIPU, DRAIN, PCNU, PR, RC, RU, T>
+impl<DRAIN, PR, PCNC, PCNU, T> PrecisionGet
+    for Builder<
+        ClipAntimeridianC<Resample<PR, PCNC, ConnectedResample<PCNC, T>, T>, T>,
+        ClipAntimeridianU<Resample<PR, PCNC, ConnectedResample<PCNC, T>, T>, T>,
+        DRAIN,
+        PCNU,
+        PR,
+        Resample<PR, PCNC, ConnectedResample<PCNC, T>, T>,
+        Resample<PR, PCNC, Unconnected, T>,
+        T,
+    >
 where
-    CLIPC: Clone,
-    CLIPU: Clone,
-    T: CoordFloat,
+    PCNC: Clone,
+    PCNU: Clone,
+    PR: Clone + Transform<T = T>,
+    T: CoordFloat + Default + FloatConst,
+{
+    type T = T;
+
+    #[inline]
+    fn precision(&self) -> T {
+        self.base.precision()
+    }
+}
+
+impl<DRAIN, PR, PCNC, PCNU, T> PrecisionGet
+    for Builder<
+        ClipCircleC<Resample<PR, PCNC, ConnectedResample<PCNC, T>, T>, T>,
+        ClipCircleU<Resample<PR, PCNC, ConnectedResample<PCNC, T>, T>, T>,
+        DRAIN,
+        PCNU,
+        PR,
+        Resample<PR, PCNC, ConnectedResample<PCNC, T>, T>,
+        Resample<PR, PCNC, Unconnected, T>,
+        T,
+    >
+where
+    PR: Clone + Transform<T = T>,
+    PCNC: Clone,
+    T: AbsDiffEq<Epsilon = T> + CoordFloat + FloatConst,
 {
     type T = T;
 
