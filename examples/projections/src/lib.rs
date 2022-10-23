8a4594a86ddb396666ea9780f7ab192d390e73af
Martin <martinfrances107@hotmail.com>
2022-10-23 20:37:37 +0100

    Minor: examples removed handcrafted console_log bindings, that exist in wasm-bindgen-test.

--- examples/projections/src/lib.rs
+++ examples/projections/src/lib.rs
@@ -18,6 +18,8 @@ use topojson::Topology;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::JsFuture;
+use wasm_bindgen_test::console_log;
+
 use web_sys::Document;
 use web_sys::*;
 
@@ -40,33 +42,6 @@ use mercator::draw_mercator;
 use mercator_transverse::draw_mercator_transverse;
 use orthographic::draw_orthographic;
 use stereographic::draw_sterographic;
-
-#[wasm_bindgen]
-#[cfg(not(tarpaulin_include))]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-
-    // The `console.log` is quite polymorphic, so we can bind it with multiple
-    // signatures. Note that we need to use `js_name` to ensure we always call
-    // `log` in JS.
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_u32(a: u32);
-
-    // Multiple arguments too!
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_many(a: &str, b: &str);
-
-    fn alert(s: &str);
-}
-
-macro_rules! console_log {
-    // Note that this is using the `log` function imported above during
-    // `bare_bones`
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
 
 #[cfg(not(tarpaulin_include))]
 fn document() -> Result<Document, JsValue> {
--- examples/ring/src/lib.rs
+++ examples/ring/src/lib.rs
@@ -6,48 +6,18 @@
 //! See the README.md.
 extern crate web_sys;
 
-mod orthographic;
-mod stereographic;
-
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_test::console_log;
 use web_sys::Document;
 use web_sys::Element;
 
 use orthographic::draw_orthographic;
 use stereographic::draw_sterographic;
 
+mod orthographic;
+mod stereographic;
+
 type Result<T> = std::result::Result<T, JsValue>;
-
-#[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-
-    // The `console.log` is quite polymorphic, so we can bind it with multiple
-    // signatures. Note that we need to use `js_name` to ensure we always call
-    // `log` in JS.
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_u32(a: u32);
-
-    // Multiple arguments too!
-    #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_many(a: &str, b: &str);
-
-    fn alert(s: &str);
-}
-
-// Next let's define a macro that's like `println!`, only it works for
-// `console.log`. Note that `println!` doesn't actually work on the wasm target
-// because the standard library currently just eats all output. To get
-// `println!`-like behavior in your app you'll likely want a macro like this.
-
-macro_rules! console_log {
-    // Note that this is using the `log` function imported above during
-    // `bare_bones`
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
 
 fn document() -> Result<Document> {
     let window = web_sys::window().unwrap();
