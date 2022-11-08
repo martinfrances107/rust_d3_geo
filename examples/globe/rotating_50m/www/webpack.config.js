2f99a3c4c04bac83a44955677835f41efbb15ad9
Martin <martinfrances107@hotmail.com>
2022-11-08 20:22:45 +0000

    Minor converted examples/globe/rotating_50 into typescript.

--- examples/globe/rotating_50m/www/webpack.config.js
+++ examples/globe/rotating_50m/www/webpack.config.js
@@ -1,15 +1,27 @@
+const zlib = require('zlib');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-
 const path = require('path')
 
 module.exports = {
-    entry: './bootstrap.js',
+    entry: './bootstrap.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bootstrap.js'
     },
+    module: {
+        rules: [{
+            test: '/.ts?$/',
+            use: 'ts-loader',
+            exclude: '/node_modules/'
+        }]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
     mode: 'production',
+    devtool: 'inline-source-map',
     experiments: { syncWebAssembly: true },
     plugins: [
         new ESLintPlugin(),
@@ -18,6 +30,20 @@ module.exports = {
                 { from: 'index.html' },
                 { from: 'public/world-atlas', to: 'world-atlas' }
             ]
-        })
+        }),
+        new CompressionPlugin({
+            filename: '[path][base].br',
+            algorithm: 'brotliCompress',
+            test: /\.(js|json|css|html)$/,
+            compressionOptions: {
+                params: {
+                    [zlib.constants.BROTLI_PARAM_QUALITY]: 11
+                }
+            },
+            threshold: 10240,
+            minRatio: 0.8,
+            deleteOriginalAssets: false
+        }),
+
     ]
 }
