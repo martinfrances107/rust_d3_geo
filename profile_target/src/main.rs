e02e25ec90918006b8a46da09cd4e3efef954611
Martin <martinfrances107@hotmail.com>
2023-02-01 21:29:57 +0000

    Added perf data and flamegraph to .gitignore.

--- profile_target/src/main.rs
+++ profile_target/src/main.rs
@@ -12,7 +12,6 @@ use num_traits::FloatConst;
 use rust_topojson_client::feature::feature_from_name;
 use topojson::Topology;
 
-use d3_geo_rs::graticule::generate_mls;
 use d3_geo_rs::path::builder::Builder as PathBuilder;
 use d3_geo_rs::projection::orthographic::Orthographic;
 use d3_geo_rs::projection::Build;
