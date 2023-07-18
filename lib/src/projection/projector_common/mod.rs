cd235742ea1f43682d050c392477bf0c44ac066a
Martin <martinfrances107@hotmail.com>
2023-07-18 08:51:00 +0100

    Minor: cosmetic changes.

--- lib/src/projection/projector_common/mod.rs
+++ lib/src/projection/projector_common/mod.rs
@@ -97,7 +97,7 @@ where
         // Populate cache.
         self.cache = Some((drain.clone(), out.clone()));
 
-        // First stage is a transform_radians node.
+        // First stage is a transform radians node.
         out
     }
 }
