f7cd76b89cd49d6ff1cc689ec8c7cff9c375274b
martinfrances107 <martinfrances107@hotmail.com>
2024-07-23 18:20:02 +0100

    Minor run cargo fmt on new test bench.

--- lib/benches/transforms.rs
+++ lib/benches/transforms.rs
@@ -12,23 +12,26 @@ use d3_geo_rs::stream::Stream;
 use geo::Coord;
 
 fn transform_loop() {
-  let ep = DrainStub::default();
-  let mut str = StreamTransformRadians::default().connect(ep);
+    let ep = DrainStub::default();
+    let mut str = StreamTransformRadians::default().connect(ep);
 
-  for i in 1..100_000{
-    let p = Coord{x: i as f32, y: i as f32};
-    str.point(&p, None);
-    assert_ne!(p, str.endpoint().last_point);
-  }
+    for i in 1..100_000 {
+        let p = Coord {
+            x: i as f32,
+            y: i as f32,
+        };
+        str.point(&p, None);
+        assert_ne!(p, str.endpoint().last_point);
+    }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-  let mut g = c.benchmark_group("transforms");
+    let mut g = c.benchmark_group("transforms");
 
-  // Increased the default run time by 3 seconds after gettings warnings that the task was taking too long.
-  g.measurement_time(Duration::from_secs(10));
+    // Increased the default run time by 3 seconds after gettings warnings that the task was taking too long.
+    g.measurement_time(Duration::from_secs(10));
 
-  g.bench_function("transforms", |b| b.iter(transform_loop));
+    g.bench_function("transforms", |b| b.iter(transform_loop));
 }
 
 criterion_group!(benches, criterion_benchmark);
