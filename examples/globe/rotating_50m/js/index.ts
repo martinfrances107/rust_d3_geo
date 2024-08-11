0cefe2b18e45b2b37b017084471a6c55785ff100
martinfrances107 <martinfrances107@hotmail.com>
2024-08-11 18:04:58 +0100

    examples/globe/rotating_50m
    * Bugfix: In the two webpack.config.js files the test function was
      buggy and was checking nothing.
    
    * Fixed npm outdated issues.
    
    * Typescript was targetting es5. Now using es6 the stas on "can I was" are almost idententical
    while the perfoamce is reporedly better.
    
    * Typescript scans against es2022.
    
    * Dropped .eslintrc.js now using tslint.json
    
    * npm run lint - restored linting, fixed up issues (dropped var, now const let etc )
    
    NB does not fix longstanding issues where under rotating a unwanted "video invert"
    effect occurs.

--- examples/globe/rotating_50m/js/index.ts
+++ examples/globe/rotating_50m/js/index.ts
@@ -1,59 +1,61 @@
-import('../pkg')
-  .then(renderer => {
-    console.log("wasm is imported");
+import { Renderer } from "../pkg";
 
-    const perf = document.getElementById('perf')
+import("../pkg").then((pkg) => {
+  console.log("wasm is imported");
 
-    if (perf != null) {
-      perf.innerHTML = 'Rwender Time: ...Calculating'
+  const perf = document.getElementById("perf");
 
-      // Holds elapsed samples (use to compute the standard deviation).
-      const elapsedArray = Array(200).fill(0)
-      // index into the elapsedArray 0..199
-      let index = 0
+  if (perf != null) {
+    perf.innerHTML = "Render Time: ...Calculating";
 
-      renderer.Renderer.new('./world-atlas/world/50m.json', 0)
-        .then((renderer) => {
-          const canvasArray = document.getElementsByTagName('canvas')
-          if (canvasArray.length !== 1) {
-            return
+    // Holds elapsed samples (use to compute the standard deviation).
+    const elapsedArray = Array(200).fill(0);
+    // index into the elapsedArray 0..199
+    let index = 0;
+
+    pkg.Renderer.new("./world-atlas/world/50m.json", 0)
+      .then((renderer: Renderer) => {
+        const canvasArray = document.getElementsByTagName("canvas");
+        if (canvasArray.length !== 1) {
+          return;
+        }
+
+        const context = canvasArray[0].getContext("2d");
+        if (context == null) {
+          return;
+        }
+
+        const renderLoop = () => {
+          context.clearRect(0, 0, 960, 600);
+          const t0 = performance.now();
+          renderer.render();
+          const t1 = performance.now();
+
+          // Compute the mean elapsed time and compute the standard deviation based on the
+          // the last 200 samples.
+          const elapsed = t1 - t0;
+          index = (index + 1) % 200;
+          elapsedArray[index] = elapsed;
+          if (index === 199) {
+            const n = elapsedArray.length;
+            const mean = elapsedArray.reduce((a, b) => a + b, 0) / n;
+            const stdDev = Math.sqrt(
+              elapsedArray
+                .map((x) => Math.pow(x - mean, 2))
+                .reduce((a, b) => a + b) / n
+            );
+            const meanString = mean.toPrecision(4);
+            const stdDevString = stdDev.toPrecision(4);
+            perf.innerHTML = `Mean Render Time: ${meanString} +/- ${stdDevString} ms`;
           }
 
-          const context = canvasArray[0].getContext('2d')
-          if (context == null) {
-            return
-          }
+          requestAnimationFrame(renderLoop);
+        };
 
-          const renderLoop = () => {
-            context.clearRect(0, 0, 960, 600)
-            const t0 = performance.now()
-            renderer.render()
-            const t1 = performance.now()
-
-            // Compute the mean elapsed time and compute the standard deviation based on the
-            // the last 200 samples.
-            const elapsed = (t1 - t0)
-            index = (index + 1) % 200
-            elapsedArray[index] = elapsed
-            if (index === 199) {
-              const n = elapsedArray.length
-              const mean = elapsedArray.reduce((a, b) => a + b, 0) / n
-              const stdDev = Math.sqrt(elapsedArray.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
-              const meanString = mean.toPrecision(4)
-              const stdDevString = stdDev.toPrecision(4)
-              perf.innerHTML = `Mean Render Time: ${meanString} +/- ${stdDevString} ms`
-            }
-
-            requestAnimationFrame(renderLoop)
-          }
-
-          renderLoop()
-        }).catch(() => { console.log('unable to construct Renderer.') })
-    }
-
-
-
-
-  })
-
-
+        renderLoop();
+      })
+      .catch(() => {
+        console.log("unable to construct Renderer.");
+      });
+  }
+});
