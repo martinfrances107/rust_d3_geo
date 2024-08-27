31cdd1e75582d039c4816f42256de6944fa90ed5
martinfrances107 <martinfrances107@hotmail.com>
2024-08-27 21:55:27 +0100

    Perf now that rotation is on the hot path.. removing the unperformant .to_radian() and to_degrees().

--- lib/src/rot/rotation.rs
+++ lib/src/rot/rotation.rs
@@ -16,6 +16,8 @@ where
     T: CoordFloat,
 {
     rotate: RotateRadians<T>,
+    frac_pi_180: T,
+    frac_180_pi: T,
 }
 
 impl<T> Rotation<T>
@@ -25,12 +27,17 @@ where
     /// Constructor.
     #[inline]
     pub fn new(delta_lambda: T, delta_phi: T, delta_gamma: T) -> Self {
+        let frac_pi_180 = T::PI() / T::from(180).unwrap();
+        let frac_180_pi = T::from(180).unwrap() / T::PI();
+
         Self {
             rotate: rotate_radians([
-                delta_lambda.to_radians(),
-                delta_phi.to_radians(),
-                delta_gamma.to_radians(),
+                delta_lambda * frac_pi_180,
+                delta_phi * frac_pi_180,
+                delta_gamma * frac_pi_180,
             ]),
+            frac_pi_180,
+            frac_180_pi,
         }
     }
 }
@@ -44,23 +51,23 @@ where
 
     fn transform(&self, coordinate: &Coord<T>) -> Coord<T> {
         let temp = self.rotate.transform(&Coord {
-            x: coordinate.x.to_radians(),
-            y: coordinate.y.to_radians(),
+            x: coordinate.x * self.frac_pi_180,
+            y: coordinate.y * self.frac_pi_180,
         });
         Coord {
-            x: temp.x.to_degrees(),
-            y: temp.y.to_degrees(),
+            x: temp.x * self.frac_180_pi,
+            y: temp.y * self.frac_180_pi,
         }
     }
 
     fn invert(&self, coordinate: &Coord<T>) -> Coord<T> {
         let temp = self.rotate.invert(&Coord {
-            x: coordinate.x.to_radians(),
-            y: coordinate.y.to_radians(),
+            x: coordinate.x * self.frac_pi_180,
+            y: coordinate.y * self.frac_pi_180,
         });
         Coord {
-            x: temp.x.to_degrees(),
-            y: temp.y.to_degrees(),
+            x: temp.x * self.frac_180_pi,
+            y: temp.y * self.frac_180_pi,
         }
     }
 }
