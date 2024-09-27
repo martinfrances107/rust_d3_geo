893ac334d53f20ddeada6e587f122d6274d8612c
martinfrances107 <martinfrances107@hotmail.com>
2024-09-27 11:49:24 +0100

    examples/globe/rotating_wpgu: artefact flashing!
    Fixed longstanding issue with strange geometry flashing
    
    The only occurred with polygons/Multipolygons.
    
    The solution was to resolve precision issues, by converting points to f64.
    Which has a performance impact.
    
    See the aditional test "hidden_square"

--- lib/src/polygon_contains.rs
+++ lib/src/polygon_contains.rs
@@ -11,14 +11,12 @@ use crate::math::EPSILON2;
 use crate::EPSILON;
 
 #[inline]
-fn longitude<T>(point: &Coord<T>) -> T
-where
-    T: CoordFloat + FloatConst,
-{
-    if point.x.abs() <= T::PI() {
+fn longitude(point: &Coord<f64>) -> f64 {
+    if point.x.abs() <= f64::PI() {
         point.x
     } else {
-        point.x.signum() * ((point.x.abs() + T::PI()) % T::TAU() - T::PI())
+        point.x.signum()
+            * ((point.x.abs() + f64::PI()) % f64::TAU() - f64::PI())
     }
 }
 
@@ -30,87 +28,92 @@ pub fn polygon_contains<T>(polygon: &[LineString<T>], point: &Coord<T>) -> bool
 where
     T: CoordFloat + FloatConst,
 {
-    let lambda = longitude(point);
-    let mut phi = point.y;
+    let lambda = longitude(&Coord {
+        x: point.x.to_f64().unwrap(),
+        y: point.y.to_f64().unwrap(),
+    });
+    let mut phi = point.y.to_f64().unwrap();
     let sin_phi = phi.sin();
     let (sin_lambda, cos_lambda) = lambda.sin_cos();
-    let normal = [sin_lambda, -cos_lambda, T::zero()];
-    let mut angle = T::zero();
+    let normal = [sin_lambda, -cos_lambda, 0_f64];
+    let mut angle = 0_f64;
 
-    let mut sum = T::zero();
+    let mut sum = 0_f64;
     let mut winding = 0_i32;
 
-    if sin_phi == T::one() {
-        phi = T::FRAC_PI_2() + T::from(EPSILON).unwrap();
-    } else if sin_phi == -T::one() {
-        phi = -T::FRAC_PI_2() - T::from(EPSILON).unwrap();
+    if sin_phi == 1_f64 {
+        phi = f64::FRAC_PI_2() + EPSILON;
+    } else if sin_phi == -1_f64 {
+        phi = -f64::FRAC_PI_2() - EPSILON;
     }
 
-    let two = T::from(2).unwrap();
-    for polygon_i in polygon {
-        let ring = polygon_i;
+    for ring in polygon {
         let m = ring.coords_count();
         if m == 0 {
             continue;
         };
 
-        let mut point0 = *ring.0.last().unwrap();
+        let mut point0 = Coord {
+            x: ring[m - 1].x.to_f64().unwrap(),
+            y: ring[m - 1].y.to_f64().unwrap(),
+        };
         let mut lambda0 = longitude(&point0);
-        let phi0 = point0.y / two + T::FRAC_PI_4();
+        let phi0 = point0.y / 2_f64 + f64::FRAC_PI_4();
         let (mut sin_phi0, mut cos_phi0) = phi0.sin_cos();
 
-        for point1 in ring.0.iter().take(m) {
-            let lambda1 = longitude(point1);
-            let phi1 = point1.y / two + T::FRAC_PI_4();
+        let mut j = 0;
+        loop {
+            if j >= m {
+                break;
+            }
+            let point1 = Coord {
+                x: ring[j].x.to_f64().unwrap(),
+                y: ring[j].y.to_f64().unwrap(),
+            };
+            let lambda1 = longitude(&point1);
+            let phi1 = point1.y / 2_f64 + f64::FRAC_PI_4();
             let (sin_phi1, cos_phi1) = phi1.sin_cos();
             let delta = lambda1 - lambda0;
             let sign = delta.signum();
             let abs_delta = sign * delta;
-            let antimeridian = abs_delta > T::PI();
+            let antimeridian = abs_delta > f64::PI();
             let k = sin_phi0 * sin_phi1;
 
-            sum = sum
-                + (k * sign * abs_delta.sin())
-                    .atan2(cos_phi0 * cos_phi1 + k * abs_delta.cos());
-            angle = angle
-                + if antimeridian {
-                    delta + sign * T::TAU()
-                } else {
-                    delta
-                };
-
+            sum += (k * sign * abs_delta.sin())
+                .atan2(cos_phi0.mul_add(cos_phi1, k * abs_delta.cos()));
+            angle += if antimeridian {
+                sign.mul_add(f64::TAU(), delta)
+            } else {
+                delta
+            };
             // Are the longitudes either side of the point’s meridian (lambda),
             // and are the latitudes smaller than the parallel (phi)?
-            // if antimeridian ^ lambda0 >= lambda ^ lambda1 >= lambda {
-            // if (antimeridian ^ lambda0 >= lambda ^ lambda1 >= lambda) {
             if antimeridian ^ (lambda0 >= lambda) ^ (lambda1 >= lambda) {
-                let mut arc = cross(&cartesian(&point0), &cartesian(point1));
+                let mut arc = cross(&cartesian(&point0), &cartesian(&point1));
                 normalize_in_place(&mut arc);
                 let mut intersection = cross(&normal, &arc);
                 normalize_in_place(&mut intersection);
-                let phi_arc: T = if antimeridian ^ (delta >= T::zero()) {
+                let phi_arc = if antimeridian ^ (delta >= 0_f64) {
                     -(intersection[2].asin())
                 } else {
                     intersection[2].asin()
                 };
-
                 if phi > phi_arc
-                    || phi == phi_arc
-                        && (arc[0] != T::zero() || arc[1] != T::zero())
+                    || (phi == phi_arc && (arc[0] != 0_f64 || arc[1] != 0_f64))
                 {
-                    if antimeridian ^ (delta >= T::zero()) {
+                    if antimeridian ^ (delta >= 0_f64) {
                         winding += 1;
                     } else {
                         winding -= 1;
                     };
                 }
             }
-
             // Loop is about the restart.
+            j += 1;
             lambda0 = lambda1;
             sin_phi0 = sin_phi1;
             cos_phi0 = cos_phi1;
-            point0 = *point1;
+            point0 = point1;
         }
     }
 
@@ -126,10 +129,73 @@ where
     // same side as the South pole.
     let is_winding_odd = winding & 1 == 1;
 
-    let epsilon = T::from(EPSILON).unwrap();
-    let epsilon2 = T::from(EPSILON2).unwrap();
     let is_south_pole_inside =
-        angle < -epsilon || angle < epsilon && sum < -epsilon2;
+        angle < -EPSILON || (angle < EPSILON && sum < -EPSILON2);
 
     is_south_pole_inside ^ is_winding_odd
+}
+
+#[cfg(test)]
+mod polygon_precision {
+
+    // This has no equivalent in the javascript version
+    //
+    /// This test addresses a longstanding issue ...
+    ///
+    /// javascript uses a Addr class to improve precision in this
+    /// polygon_contains.
+    ///
+    /// I dicovered artifacts appearing/flashing in `examples/globe/rotating_wgpu`
+    /// traced to this function. When the angle values are close zero
+    /// this was occassionaly tripping up the computation of `is_south_pole_inside`
+    /// [ -EPSILON<angle<EPSILON ]
+    ///
+    /// This issue only appears when streaming `MultiPolygons/Polygons`
+    ///
+    /// `polygon_contains()` was returning TRUE not FALSE.
+    ///
+    /// I tracked down the issue by pulling code from examples/ring
+    /// and then simplifying the polygon to the simplests geometry that still
+    /// flashed
+    ///
+    /// "A simple square (closed ring described by 5 points)"
+    ///
+    /// ( the flashing occurred when the normal for the ring pointed directly away
+    /// from the observer).
+    use geo::LineString;
+    use geo_types::Coord;
+
+    use crate::polygon_contains::polygon_contains;
+
+    #[test]
+    fn hidden_square() {
+        let ring = [LineString::<f32>::from(vec![
+            Coord {
+                x: 2.6974134,
+                y: 0.7027946,
+            },
+            Coord {
+                x: 2.8178122,
+                y: 0.935154,
+            },
+            Coord {
+                x: -3.1275373,
+                y: 0.8155085,
+            },
+            Coord {
+                x: 2.9835172,
+                y: 0.60618496,
+            },
+            Coord {
+                x: 2.6974132,
+                y: 0.7027947,
+            },
+        ])];
+
+        let point = Coord::<f32> {
+            x: -3.1415927,
+            y: -1.5707964,
+        };
+        assert!(!polygon_contains(&ring, &point));
+    }
 }
