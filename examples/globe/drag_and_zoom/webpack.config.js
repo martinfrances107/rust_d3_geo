d5052c6f6d86e8d80ac53dbaf567ecb4cc49a15e
martinfrances107 <martinfrances107@hotmail.com>
2025-01-14 10:47:12 +0000

    Updated examples/drag_and_zoom

--- examples/globe/drag_and_zoom/webpack.config.js
+++ examples/globe/drag_and_zoom/webpack.config.js
@@ -1,45 +1,46 @@
-const path = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const ESLintPlugin = require('eslint-webpack-plugin')
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
-  entry: './js/index.ts',
+  entry: "./js/index.ts",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
   },
   module: {
-    rules: [{
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: '/node_modules/'
-    }]
+    rules: [
+      {
+        test: /\.ts/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
+    ],
   },
   performance: {
     maxEntrypointSize: 1 * 1024 * 1024,
-    maxAssetSize: 1 * 1024 * 1024
+    maxAssetSize: 1 * 1024 * 1024,
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: [".tsx", ".ts", ".js"],
   },
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   experiments: { syncWebAssembly: true },
   plugins: [
     // new ESLintPlugin(),
     new CopyWebpackPlugin({
       patterns: [
-        { from: 'index.html' },
-        { from: 'public/world-atlas', to: 'world-atlas' }
-      ]
+        { from: "index.html" },
+        { from: "public/world-atlas", to: "world-atlas" },
+      ],
     }),
     new WasmPackPlugin({
       crateDirectory: __dirname,
-      args: '--log-level warn',
-      extraArgs: '',
-      forceMode: 'development'
+      args: "--log-level warn",
+      extraArgs: "",
+      forceMode: "development",
     }),
-
-  ]
-}
+  ],
+};
